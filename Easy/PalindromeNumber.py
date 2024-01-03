5cc97e5a0c4db8e5d5a0c8b8fb8105a67a0a5fa4
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2024-01-03 17:58:46 +0300

    Update PalindromeNumber.py

--- Easy/PalindromeNumber.py
+++ Easy/PalindromeNumber.py
@@ -1,3 +1,13 @@
 """
 Given an integer x, return true if x is a palindrome, and false otherwise.
 """
+
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        s_x = str(x)
+        return s_x == s_x[::-1]
+      
