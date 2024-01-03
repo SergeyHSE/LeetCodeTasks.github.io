c82aff765a4c013af3d826021a8a8d7edf8d9ed3
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2024-01-03 18:01:47 +0300

    Update RomanToInteger.py

--- Easy/RomanToInteger.py
+++ Easy/RomanToInteger.py
@@ -2,3 +2,27 @@
 Given a roman numeral, convert it to an integer.
 """
 
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        r_d = {
+            'I' : 1,
+            'V' : 5,
+            'X' : 10,
+            'L' : 50,
+            'C' : 100,
+            'D' : 500,
+            'M' : 1000}        
+        r = 0
+        v_p = 0
+        for i in s:
+            v = r_d[i]
+            if v > v_p:
+                r += v - 2 * v_p
+            else:
+                r += v
+            v_p = v
+        return r
