4c87b71163986c3ee20e7e0fffff539dcd41666e
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2024-01-04 15:55:47 +0300

    Update Prefix.py

--- Easy/Prefix.py
+++ Easy/Prefix.py
@@ -3,3 +3,20 @@ Write a function to find the longest common prefix string amongst an array of st
 
 If there is no common prefix, return an empty string "".
 """
+
+class Solution(object):
+    def longestCommonPrefix(self, s):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not s: 
+            return ""
+
+        f_s = min(s, key=len) 
+        for i in range(len(f_s)):
+            for j in s:
+                if f_s[i] != j[i]:
+                    return f_s[:i]
+
+        return f_s 
