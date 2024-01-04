2c43fef1af49219450f7aa003240a2c74bc6c376
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2024-01-04 15:57:51 +0300

    Update Parentheses.py

--- Easy/Parentheses.py
+++ Easy/Parentheses.py
@@ -7,3 +7,23 @@ Open brackets must be closed by the same type of brackets.
 Open brackets must be closed in the correct order.
 Every close bracket has a corresponding open bracket of the same type.
 """
+
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        st = []
+        m = {
+            ')' : '(',
+            '}' : '{',
+            ']' : '['
+        }
+        for i in s:
+            if i in m.values():
+                st.append(i)
+            elif i in m.keys():
+                if not st or st.pop() != m[i]:
+                    return False   
+        return not st
