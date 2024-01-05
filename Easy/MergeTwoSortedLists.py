586c5887513136c50714d762cc7d7345aa8aa56b
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2024-01-05 16:25:35 +0300

    Update MergeTwoSortedLists.py

--- Easy/MergeTwoSortedLists.py
+++ Easy/MergeTwoSortedLists.py
@@ -5,3 +5,34 @@ Merge the two lists into one sorted list. The list should be made by splicing to
 
 Return the head of the merged linked list.
 """
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        d = ListNode()
+        c = d
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                c.next = list1
+                list1 = list1.next
+            else:
+                c.next = list2
+                list2 = list2.next
+
+            c = c.next
+
+        if list1:
+            c.next = list1
+        elif list2:
+            c.next = list2
+
+        return d.next
