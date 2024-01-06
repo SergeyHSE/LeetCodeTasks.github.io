c615f5310d22259310ee0f9ae0fe8cd2137c5538
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2024-01-06 15:51:12 +0300

    Update InsertPosition.py

--- Easy/InsertPosition.py
+++ Easy/InsertPosition.py
@@ -3,3 +3,21 @@ Given a sorted array of distinct integers and a target value, return the index i
 
 You must write an algorithm with O(log n) runtime complexity.
 """
+
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            m = left + (right - left) // 2
+            if nums[m] == target:
+                return m
+            elif nums[m] < target:
+                left = m + 1
+            else:
+                right = m - 1
+        return left
