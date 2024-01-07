dfc8a495ccffad20cddbae8f9138e99ba2b9d377
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2024-01-07 17:22:43 +0300

    Update Sqrt.py

--- Easy/Sqrt.py
+++ Easy/Sqrt.py
@@ -5,3 +5,16 @@ You must not use any built-in exponent function or operator.
 
 For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
 """
+
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x == 1:
+            return 1
+        result = x // 2
+        while result * result > x:
+            result = (result + x // result) // 2
+        return result
