4918a2dd51394606598dbc78ff48c712d186d773
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2024-01-09 17:49:35 +0300

    Update SameTree.py

--- Easy/SameTree.py
+++ Easy/SameTree.py
@@ -30,3 +30,32 @@ class Solution(object):
             return False
         else:
             return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right
+
+                                                                       
+#########################
+#     without recursion #
+#########################
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        stack = [(p, q)]
+        while stack:
+            node_p, node_q = stack.pop()
+            if not node_p and not node_q:
+                continue
+            if not node_p  or not node_q or node_p.val != node_q.val:
+                return False
+            stack.append((node_p.left, node_q.left))
+            stack.append((node_p.right, node_q.right))
+        return True
