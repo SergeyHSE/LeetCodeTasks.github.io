b41a30cbc157a9396dfded1825a9397cbd01f310
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2024-01-10 21:05:27 +0300

    Update BalancedBinaryTree.py

--- Easy/BalancedBinaryTree.py
+++ Easy/BalancedBinaryTree.py
@@ -2,3 +2,29 @@
 Given a binary tree, determine if it is 
 height-balanced.
 """
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isBalanced(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        def check_balance(node):
+            if not node:
+                return 0
+            
+            left_height = check_balance(node.left)
+            right_height = check_balance(node.right)
+            
+            if left_height == -1 or right_height == -1 or abs(left_height - right_height) > 1:
+                return -1
+            
+            return 1 + max(left_height, right_height)
+        
+        return check_balance(root) != -1
