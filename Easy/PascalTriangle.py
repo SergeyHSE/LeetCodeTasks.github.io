0fcc13d022dd1f50e5838101f637a3b3a26c97cb
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2024-01-11 20:55:53 +0300

    Update PascalTriangle.py

--- Easy/PascalTriangle.py
+++ Easy/PascalTriangle.py
@@ -1,3 +1,15 @@
 """
 Given an integer numRows, return the first numRows of Pascal's triangle.
 """
+
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        result = [[1] * (i + 1) for i in range(numRows)]
+        for i in range(2, numRows):
+            for j in range(1, i):
+                result[i][j] = result[i - 1][j - 1] + result[i - 1][j]
+        return result
