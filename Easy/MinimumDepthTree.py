37d807c695151a8668bcfc6a9bf66fec4719ca28
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2024-01-11 19:21:30 +0300

    Update MinimumDepthTree.py

--- Easy/MinimumDepthTree.py
+++ Easy/MinimumDepthTree.py
@@ -5,3 +5,27 @@ The minimum depth is the number of nodes along the shortest path from the root n
 
 Note: A leaf is a node with no children.
 """
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def minDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        queue = deque([(root, 1)])
+        while queue:
+            node, depth = queue.popleft()
+            if not node.left and not node.right:
+                return depth
+            if node.left:
+                queue.append((node.left, depth + 1))
+            if node.right:
+                queue.append((node.right, depth + 1))
