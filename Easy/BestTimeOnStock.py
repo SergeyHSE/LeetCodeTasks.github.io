b12b331a0df45f4809e47ae96e54b4b7c38b41e3
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2024-01-12 13:17:24 +0300

    Update BestTimeOnStock.py

--- Easy/BestTimeOnStock.py
+++ Easy/BestTimeOnStock.py
@@ -4,3 +4,15 @@ You want to maximize your profit by choosing a single day to buy one stock and c
 Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
 """
 
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        max_profit = 0
+        min_price = float('inf')
+        for price in prices:
+            min_price = min(min_price, price)
+            max_profit = max(max_profit, price - min_price)
+        return max_profit
