b9584458d2e66cf7d7c4839d3e5f050a82635ced
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2024-01-12 12:38:32 +0300

    Update PascalTriangle2.py

--- Easy/PascalTriangle2.py
+++ Easy/PascalTriangle2.py
@@ -1,3 +1,15 @@
 """
 Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's triangle.
 """
+
+class Solution(object):
+    def getRow(self, rowIndex):
+        """
+        :type rowIndex: int
+        :rtype: List[int]
+        """
+        result = [1] * (rowIndex + 1)
+        for i in range(1, rowIndex):
+            for j in range(i, 0, -1):
+                result[j] += result[j - 1]
+        return result
