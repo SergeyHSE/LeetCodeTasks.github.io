41ac05fd67ac1c9468d6bff0a84a8eb5344e46c5
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2024-01-13 14:03:57 +0300

    Update LinkedListCycle.py

--- Easy/LinkedListCycle.py
+++ Easy/LinkedListCycle.py
@@ -4,3 +4,25 @@ There is a cycle in a linked list if there is some node in the list that can be 
 Return true if there is a cycle in the linked list. Otherwise, return false.
 """
 
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        if not head or not head.next:
+            return False
+        slow = head
+        fast = head.next
+        while slow != fast:
+            if not fast or not fast.next:
+                return False
+            slow = slow.next
+            fast = fast.next.next
+        return True
