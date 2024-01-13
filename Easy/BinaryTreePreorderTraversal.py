1b1a262cc428cb561dff016a9994f793cbb9a944
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2024-01-13 14:37:55 +0300

    Update BinaryTreePreorderTraversal.py

--- Easy/BinaryTreePreorderTraversal.py
+++ Easy/BinaryTreePreorderTraversal.py
@@ -2,3 +2,24 @@
 Given the root of a binary tree, return the preorder traversal of its nodes' values.
 """
 
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        result= []
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if node:
+                result.append(node.val)
+                stack.append(node.right)
+                stack.append(node.left)
+        return result
