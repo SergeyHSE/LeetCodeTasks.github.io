d4c815db28e497e43937c564e6fed5cca3303a4a
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2024-01-14 12:12:15 +0300

    Update BinaryTreePostorderTraversal.py

--- Easy/BinaryTreePostorderTraversal.py
+++ Easy/BinaryTreePostorderTraversal.py
@@ -2,3 +2,24 @@
 Given the root of a binary tree, return the postorder traversal of its nodes' values.
 """
 
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def postorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        result = []
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if node:
+                result.append(node.val)
+                stack.append(node.left)
+                stack.append(node.right)
+        return result[::-1]
