6291ab7c900e8d1b8f6b4b810e80feec053bfd67
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2024-01-14 13:12:47 +0300

    Update ExcelShitColumnTitle.py

--- Easy/ExcelShitColumnTitle.py
+++ Easy/ExcelShitColumnTitle.py
@@ -2,3 +2,15 @@
 Given an integer columnNumber, return its corresponding column title as it appears in an Excel sheet.
 """
 
+class Solution(object):
+    def convertToTitle(self, columnNumber):
+        """
+        :type columnNumber: int
+        :rtype: str
+        """
+        result = []
+        while columnNumber:
+            columnNumber -= 1
+            result.append(chr(columnNumber % 26 + ord('A')))
+            columnNumber //= 26
+        return ''.join(reversed(result))
