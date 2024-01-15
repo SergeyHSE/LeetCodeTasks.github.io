0a270f5f5f7078d3d0d236ac2cdff0752bc9a711
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2024-01-15 18:37:43 +0300

    Update MajorityElement.py

--- Easy/MajorityElement.py
+++ Easy/MajorityElement.py
@@ -26,3 +26,20 @@ class Solution(object):
         count = Counter(nums)
         majority_element = max(count, key=count.get)
         return majority_element
+
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        counts = {}
+        for num in nums:
+            if num in counts:
+                counts[num] +=1
+            else:
+                counts[num] = 1
+        majority_count = len(nums) // 2
+        for num, count in counts.items():
+            if count > majority_count:
+                return num
