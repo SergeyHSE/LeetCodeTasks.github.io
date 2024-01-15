05b99e08666a80939821c6eecaa1c2f8baebf85e
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2024-01-15 17:57:30 +0300

    Update ReverceBits.py

--- Easy/ReverceBits.py
+++ Easy/ReverceBits.py
@@ -1,3 +1,13 @@
 """
 Reverse bits of a given 32 bits unsigned integer.
 """
+
+class Solution:
+    # @param n, an integer
+    # @return an integer
+    def reverseBits(self, n):
+        binary_str = bin(n)[2:].zfill(32)
+        reversed_str = binary_str[::-1]
+        reversed_n = int(reversed_str, 2)
+        return reversed_n
+      
