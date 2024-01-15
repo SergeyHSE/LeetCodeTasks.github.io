3e840553a9d2d9a75bcac3c2229d7e7808d622be
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2024-01-15 17:56:14 +0300

    Update ExcelSheetColumnNumber.py

--- Easy/ExcelSheetColumnNumber.py
+++ Easy/ExcelSheetColumnNumber.py
@@ -2,3 +2,14 @@
 Given a string columnTitle that represents the column title as appears in an Excel sheet, return its corresponding column number.
 """
 
+class Solution(object):
+    def titleToNumber(self, columnTitle):
+        """
+        :type columnTitle: str
+        :rtype: int
+        """
+        result = 0
+        for i in columnTitle:
+            result = result * 26 + ord(i) - ord('A') + 1
+        return result
+        
