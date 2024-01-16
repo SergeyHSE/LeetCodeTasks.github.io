ecb3bacf0fbceeec4c8cf1ab71125ff043e9df7f
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2024-01-16 14:29:29 +0300

    Update HappyNumber.py

--- Easy/HappyNumber.py
+++ Easy/HappyNumber.py
@@ -7,3 +7,16 @@ Those numbers for which this process ends in 1 are happy.
 Return true if n is a happy number, and false if not.
 """
 
+class Solution(object):
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        def get_next(num):
+            return sum(int(digit) ** 2 for digit in str(num))
+        seen = set()
+        while n != 1 and n not in seen:
+            seen.add(n)
+            n = get_next(n)
+        return n == 1
