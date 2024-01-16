ce0b2570effbc64ef85e5a647f312d56bf8bb5e9
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2024-01-16 16:42:11 +0300

    Update NewHeadTree.py

--- Easy/NewHeadTree.py
+++ Easy/NewHeadTree.py
@@ -26,3 +26,34 @@ class Solution(object):
                 current = current.next
 
         return head
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def removeElements(self, head, val):
+        """
+        :type head: ListNode
+        :type val: int
+        :rtype: ListNode
+        """
+        while head and head.val == val:
+            head = head.next
+
+        current = head
+        prev = None
+
+        while current:
+            if current.val == val:
+                if prev:
+                    prev.next = current.next
+                else:
+                    head = current.next
+            else:
+                prev = current
+
+            current = current.next
+
+        return head
