2a2bef9155ce3ffbb58d1dce1d05a2e82ab14879
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2024-01-16 16:03:45 +0300

    Update NewHeadTree.py

--- Easy/NewHeadTree.py
+++ Easy/NewHeadTree.py
@@ -2,3 +2,27 @@
 Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
 """
 
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def removeElements(self, head, val):
+        """
+        :type head: ListNode
+        :type val: int
+        :rtype: ListNode
+        """
+        while head and head.val == val:
+            head = head.next
+
+        current = head
+
+        while current:
+            if current.next and current.next.val == val:
+                current.next = current.next.next
+            else:
+                current = current.next
+
+        return head
