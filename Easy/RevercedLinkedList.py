cc1a9f49b792809844525044deea3af5fefc871b
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2024-01-17 17:46:26 +0300

    Update RevercedLinkedList.py

--- Easy/RevercedLinkedList.py
+++ Easy/RevercedLinkedList.py
@@ -22,3 +22,18 @@ class Solution(object):
             prev = current
             current = next_node
         return prev
+
+
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head or not head.next:
+            return head
+        
+        reversed_head = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return reversed_head
