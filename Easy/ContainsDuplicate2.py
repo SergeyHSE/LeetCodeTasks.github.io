93749554358525830b9404584c514e9ac91a036b
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2024-01-18 19:24:10 +0300

    Update ContainsDuplicate2.py

--- Easy/ContainsDuplicate2.py
+++ Easy/ContainsDuplicate2.py
@@ -2,3 +2,16 @@
 Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
 """
 
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        index_map = {}
+        for i, num in enumerate(nums):
+            if num in index_map and i - index_map[num] <= k:
+                return True
+            index_map[num] = i
+        return False      
