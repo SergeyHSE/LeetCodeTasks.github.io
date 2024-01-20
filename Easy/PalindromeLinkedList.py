b9cdaf962a9f32c611d01097ff9c0b3bb5a9bb3c
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2024-01-20 15:49:17 +0300

    Update PalindromeLinkedList.py

--- Easy/PalindromeLinkedList.py
+++ Easy/PalindromeLinkedList.py
@@ -3,3 +3,20 @@ Given the head of a singly linked list, return true if it is a
 palindrome or false otherwise.
  """
 
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def isPalindrome(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        values = []
+        current = head
+        while current:
+            values.append(current.val)
+            current = current.next
+        return values == values[::-1]
