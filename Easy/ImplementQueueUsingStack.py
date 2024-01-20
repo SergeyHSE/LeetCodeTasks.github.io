d6ef31d0c89fbccf250c78eea032ab410c5bb92e
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2024-01-20 15:33:47 +0300

    Update ImplementQueueUsingStack.py

--- Easy/ImplementQueueUsingStack.py
+++ Easy/ImplementQueueUsingStack.py
@@ -10,3 +10,50 @@ You must use only standard operations of a stack, which means only push to top, 
 Depending on your language, the stack may not be supported natively. You may simulate a stack using a list or deque (double-ended queue) as long as you use only a stack's standard operations.
 """
 
+class MyQueue(object):
+
+    def __init__(self):
+        self.stack_enqueue = []
+        self.stack_dequeue = []
+        
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: None
+        """
+        self.stack_enqueue.append(x)
+        
+
+    def pop(self):
+        """
+        :rtype: int
+        """
+        if not self.stack_dequeue:
+            while self.stack_enqueue:
+                self.stack_dequeue.append(self.stack_enqueue.pop())
+        return self.stack_dequeue.pop()
+        
+
+    def peek(self):
+        """
+        :rtype: int
+        """
+        if not self.stack_dequeue:
+            while self.stack_enqueue:
+                self.stack_dequeue.append(self.stack_enqueue.pop())
+        return self.stack_dequeue[-1]
+        
+
+    def empty(self):
+        """
+        :rtype: bool
+        """
+        return not self.stack_enqueue and not self.stack_dequeue
+      
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
