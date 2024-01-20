080fdd5b33e5307ef0c722f61319b581ba8952ca
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2024-01-20 16:58:05 +0300

    Update ValidAnagram.py

--- Easy/ValidAnagram.py
+++ Easy/ValidAnagram.py
@@ -3,3 +3,23 @@ Given two strings s and t, return true if t is an anagram of s, and false otherw
 An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
 """
 
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+        
+        char_count = {}
+        
+        for char in s:
+            char_count[char] = char_count.get(char, 0) + 1
+            
+        for char in t:
+            if char not in char_count or char_count[char] == 0:
+                return False
+            char_count[char] -= 1
+        return True
