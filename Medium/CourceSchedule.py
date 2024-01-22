2c600f24c7e2f55a7245e435fa7871e4131a34b3
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2024-01-22 12:18:04 +0300

    Update CourceSchedule.py

--- Medium/CourceSchedule.py
+++ Medium/CourceSchedule.py
@@ -4,3 +4,32 @@ For example, the pair [0, 1], indicates that to take course 0 you have to first 
 Return true if you can finish all courses. Otherwise, return false.
 """
 
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        """
+        graph = {i: [] for i in range(numCourses)}
+        for course, prereq in prerequisites:
+            graph[course].append(prereq)
+        
+        def hasCycle(course, visited):
+            if visited[course] == 1:
+                return True
+            
+            if visited[course] == -1:
+                return False
+            visited[course] = 1
+            
+            for neighbor in graph[course]:
+                if hasCycle(neighbor, visited):
+                    return True
+            visited[course] = -1
+            return False
+        visited = [0] * numCourses
+        for i in range(numCourses):
+            if not visited[i] and hasCycle(i, visited):
+                return False
+        return True
