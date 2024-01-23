276b87501fda7d074bc34bb879bf0cc69c0a850b
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2024-01-23 12:09:21 +0300

    Update UglyNumber.py

--- Easy/UglyNumber.py
+++ Easy/UglyNumber.py
@@ -3,3 +3,15 @@ An ugly number is a positive integer whose prime factors are limited to 2, 3, an
 Given an integer n, return true if n is an ugly number.
 """
 
+class Solution(object):
+    def isUgly(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n <= 0:
+            return False
+        for factor in [2, 3, 5]:
+            while n % factor == 0:
+                n //= factor
+        return n == 1
