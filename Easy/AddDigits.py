2fa0b478dc1b354ff36c52a0a1194e7e867570ff
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2024-01-23 12:07:57 +0300

    Update AddDigits.py

--- Easy/AddDigits.py
+++ Easy/AddDigits.py
@@ -2,3 +2,15 @@
 Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
 """
 
+class Solution(object):
+    def addDigits(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        if num == 0:
+            return 0
+        elif num % 9 == 0:
+            return 9
+        else:
+            return num % 9
