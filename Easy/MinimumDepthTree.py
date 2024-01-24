8b9c3e7be935c304882e057ce99ef679ebc6b081
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2024-01-24 21:33:11 +0300

    Update MinimumDepthTree.py

--- Easy/MinimumDepthTree.py
+++ Easy/MinimumDepthTree.py
@@ -29,3 +29,30 @@ class Solution(object):
                 queue.append((node.left, depth + 1))
             if node.right:
                 queue.append((node.right, depth + 1))
+
+#############################################################
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def minDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        if not root.left and not root.right:
+            return 1
+        left_depth = self.minDepth(root.left)
+        right_depth = self.minDepth(root.right)
+
+        if not root.left or not root.right:
+            return left_depth + right_depth + 1
+        
+        return min(left_depth, right_depth) + 1 
