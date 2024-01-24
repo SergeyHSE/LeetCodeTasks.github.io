8cdd51cafe80fd572e944defabfe214f292a75df
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2024-01-24 21:58:10 +0300

    Update AddTwoNumbers.py

--- Medium/AddTwoNumbers.py
+++ Medium/AddTwoNumbers.py
@@ -3,3 +3,36 @@ You are given two non-empty linked lists representing two non-negative integers.
 You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 """
 
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        dummy = ListNode()  # Dummy node to simplify code
+        current = dummy
+        carry = 0
+
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+
+            total = val1 + val2 + carry
+            carry = total // 10
+            digit = total % 10
+
+            current.next = ListNode(digit)
+            current = current.next
+
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+
+        return dummy.next 
