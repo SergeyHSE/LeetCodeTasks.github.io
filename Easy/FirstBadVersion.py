b167597c8359719740ed14d435ed6af918fdec83
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2024-02-03 21:50:56 +0300

    Update FirstBadVersion.py

--- Easy/FirstBadVersion.py
+++ Easy/FirstBadVersion.py
@@ -4,3 +4,26 @@ Suppose you have n versions [1, 2, ..., n] and you want to find out the first ba
 You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
 """
 
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+class Solution(object):
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        left, right = 1, n
+        
+        while left < right:
+            mid = (left + right) // 2
+            
+            if isBadVersion(mid):
+                right = mid
+            else:
+                left = mid + 1
+            
+        return left
+      
