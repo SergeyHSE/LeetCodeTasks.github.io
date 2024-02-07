869133b5c9aa7c8e85f773ff2f01c4af77466eca
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2024-02-07 11:16:10 +0300

    Update WordPattern.py

--- Easy/WordPattern.py
+++ Easy/WordPattern.py
@@ -2,3 +2,25 @@
 Given a pattern and a string s, find if s follows the same pattern.
 Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s.
 """
+
+class Solution(object):
+    def wordPattern(self, pattern, s):
+        """
+        :type pattern: str
+        :type s: str
+        :rtype: bool
+        """
+        words = s.split()
+        if len(pattern) != len(words):
+            return False
+        
+        char_to_word = {}
+        for char, word in zip(pattern, words):
+            if char in char_to_word:
+                if char_to_word[char] != word:
+                    return False
+            else:
+                if word in char_to_word.values():
+                    return False
+                char_to_word[char] = word
+        return True
