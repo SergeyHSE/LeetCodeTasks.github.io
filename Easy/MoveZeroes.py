db809c37b158643b87bf3a2ffe41e69148a9a07c
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2024-02-07 11:08:50 +0300

    Update MoveZeroes.py

--- Easy/MoveZeroes.py
+++ Easy/MoveZeroes.py
@@ -3,3 +3,15 @@ Given an integer array nums, move all 0's to the end of it while maintaining the
 Note that you must do this in-place without making a copy of the array.
 """
 
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        non_zero_index = 0
+        
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[i], nums[non_zero_index] = nums[non_zero_index], nums[i]
+                non_zero_index += 1
