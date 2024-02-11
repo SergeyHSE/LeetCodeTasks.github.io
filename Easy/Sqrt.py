2f38eafb3e1bba65c6e73a94223889ed9339f17d
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2024-02-11 13:28:16 +0300

    Update Sqrt.py

--- Easy/Sqrt.py
+++ Easy/Sqrt.py
@@ -18,3 +18,20 @@ class Solution(object):
         while result * result > x:
             result = (result + x // result) // 2
         return result
+
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        left, right = 0, x
+        while left <= right:
+            mid = (left + right) // 2
+            if mid * mid < x:
+                left = mid + 1
+            elif mid * mid > x:
+                right = mid - 1
+            else:
+                return mid
+        return right
