9fd4eb6c839ca707887950aca53027616556a497
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2024-02-20 18:15:06 +0300

    Update FibonacciNumber.py

--- Easy/FibonacciNumber.py
+++ Easy/FibonacciNumber.py
@@ -5,3 +5,20 @@ F(0) = 0, F(1) = 1
 F(n) = F(n - 1) + F(n - 2), for n > 1.
 Given n, calculate F(n).
 """
+class Solution(object):
+    def fib(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 0:
+            return 0
+        elif n == 1:
+            return 1
+        prev, curr = 0, 1
+
+        for _ in range(2, n + 1):
+            temp = curr
+            curr = prev + curr
+            prev = temp
+        return curr
