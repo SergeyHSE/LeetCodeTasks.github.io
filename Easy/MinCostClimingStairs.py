5755097691b65fbeac3960f7d4b78b057338b2a0
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2024-02-21 18:11:24 +0300

    Update MinCostClimingStairs.py

--- Easy/MinCostClimingStairs.py
+++ Easy/MinCostClimingStairs.py
@@ -3,3 +3,18 @@ You are given an integer array cost where cost[i] is the cost of ith step on a s
 You can either start from the step with index 0, or the step with index 1.
 Return the minimum cost to reach the top of the floor.
 """
+class Solution(object):
+    def minCostClimbingStairs(self, cost):
+        """
+        :type cost: List[int]
+        :rtype: int
+        """
+        n = len(cost)
+        dp = [0] * n
+        dp[0] = cost[0]
+        dp[1] = cost[1]
+
+        for i in range(2, n):
+            dp[i] = cost[i] + min(dp[i - 1], dp[i - 2])
+        return min(dp[-1], dp[-2])
+        
