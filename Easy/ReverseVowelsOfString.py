1b9e4e31cd6163b4dac04cab1ca3151aa6994cf5
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2024-02-27 12:34:32 +0300

    Update ReverseVowelsOfString.py

--- Easy/ReverseVowelsOfString.py
+++ Easy/ReverseVowelsOfString.py
@@ -2,3 +2,22 @@
 Given a string s, reverse only all the vowels in the string and return it.
 The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
 """
+
+class Solution(object):
+    def reverseVowels(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        vowels = 'aeiou'
+        s = list(s)
+        i, j = 0, len(s) - 1
+        while i < j:
+            while i < j and s[i].lower() not in vowels:
+                i += 1
+            while i < j and s[j].lower() not in vowels:
+                j -= 1
+            s[i], s[j] = s[j], s[i]
+            i += 1
+            j -= 1
+        return ''.join(s)
