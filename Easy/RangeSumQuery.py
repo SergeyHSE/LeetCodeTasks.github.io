8d1abdeccbc83377d351e0f7ede36d302d7e69c7
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2024-02-27 12:29:43 +0300

    Update RangeSumQuery.py

--- Easy/RangeSumQuery.py
+++ Easy/RangeSumQuery.py
@@ -5,3 +5,27 @@ Implement the NumArray class:
 NumArray(int[] nums) Initializes the object with the integer array nums.
 int sumRange(int left, int right) Returns the sum of the elements of nums between indices left and right inclusive (i.e. nums[left] + nums[left + 1] + ... + nums[right]).
 """
+
+class NumArray(object):
+
+    def __init__(self, nums):
+        """
+        :type nums: List[int]
+        """
+        self.prefix_sum = [0]
+        current_sum = 0
+        for num in nums:
+            current_sum += num
+            self.prefix_sum.append(current_sum)
+
+    def sumRange(self, left, right):
+        """
+        :type left: int
+        :type right: int
+        :rtype: int
+        """
+        return self.prefix_sum[right + 1] - self.prefix_sum[left]
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(left,right)
