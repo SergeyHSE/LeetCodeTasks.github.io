c37a22d197953a5f107b97808f6d10a2ef0e6dcb
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2024-02-27 12:31:46 +0300

    Update PowerOfThree.py

--- Easy/PowerOfThree.py
+++ Easy/PowerOfThree.py
@@ -3,3 +3,15 @@ Given an integer n, return true if it is a power of three. Otherwise, return fal
 An integer n is a power of three, if there exists an integer x such that n == 3x.
 """
 
+class Solution(object):
+    def isPowerOfThree(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n <=0:
+            return False
+        while n % 3 == 0:
+            n //= 3
+        return n == 1
+
