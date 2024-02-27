feaf2ec932593a1f7e8939ceeb31c12ca74dab77
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2024-02-27 12:30:43 +0300

    Update PowerOfFour.py

--- Easy/PowerOfFour.py
+++ Easy/PowerOfFour.py
@@ -2,3 +2,15 @@
 Given an integer n, return true if it is a power of four. Otherwise, return false.
 An integer n is a power of four, if there exists an integer x such that n == 4x.
 """
+
+class Solution(object):
+    def isPowerOfFour(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n <= 0:
+            return False
+        while n % 4 == 0:
+            n //= 4
+        return n == 1
