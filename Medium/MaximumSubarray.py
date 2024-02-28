ec9fcf7156537ecd30b48ed84d846af3d77baca4
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2024-02-28 11:09:47 +0300

    Create MaximumSubarray.py

---
+++ Medium/MaximumSubarray.py
@@ -0,0 +1,17 @@
+"""
+Given an integer array nums, find the subarray with the largest sum, and return its sum.
+"""
+
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        max_sum = current_sum = nums[0]
+        for num in nums[1:]:
+            current_sum = max(num, current_sum + num)
+            max_sum = max(max_sum, current_sum)
+        return max_sum
