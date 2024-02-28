f405c4b24eb006128b29c97907b2a8e271b48fdc
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2024-02-28 11:11:02 +0300

    Update JumpGame.py

--- Medium/JumpGame.py
+++ Medium/JumpGame.py
@@ -3,3 +3,19 @@ You are given an integer array nums. You are initially positioned at the array's
 Return true if you can reach the last index, or false otherwise.
 """
 
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        n = len(nums)
+        max_reach = 0
+        for i in range(n):
+            if i > max_reach:
+                return False
+            max_reach = max(max_reach, i + nums[i])
+            if max_reach >= n - 1:
+                return True
+        return False
+        
