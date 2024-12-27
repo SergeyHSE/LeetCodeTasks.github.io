a97bf84c2e9bd2950b68413d5e7373836d297405
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2024-12-27 13:45:38 +0300

    Update SameTree.py

--- Easy/SameTree.py
+++ Easy/SameTree.py
@@ -29,7 +29,7 @@ class Solution(object):
         elif p.val != q.val:
             return False
         else:
-            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
 
                                                                        
 #########################
