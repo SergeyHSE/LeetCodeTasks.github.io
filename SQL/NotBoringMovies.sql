56602ac6723a48211f62c97e7fa854a56d5aa310
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2025-01-03 20:05:42 +0300

    Update NotBoringMovies.sql

--- SQL/NotBoringMovies.sql
+++ SQL/NotBoringMovies.sql
@@ -1,3 +1,4 @@
+'''
 Table: Cinema
 
 +----------------+----------+
@@ -16,8 +17,9 @@ rating is a 2 decimal places float in the range [0, 10]
 Write a solution to report the movies with an odd-numbered ID and a description that is not "boring".
 
 Return the result table ordered by rating in descending order.
+'''
 
-The result format is in the following example.
+# The result format is in the following example.
 
 # Write your MySQL query statement below
 select id, movie, description, rating
