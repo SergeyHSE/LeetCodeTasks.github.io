259475390b2ec8b520370f378c970e2ab44ab4e7
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2025-01-06 17:46:12 +0300

    Create SalesAnalisys.sql

---
+++ SQL/SalesAnalisys.sql
@@ -0,0 +1,45 @@
+'''
+Table: Product
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| product_id   | int     |
+| product_name | varchar |
+| unit_price   | int     |
++--------------+---------+
+product_id is the primary key (column with unique values) of this table.
+Each row of this table indicates the name and the price of each product.
+Table: Sales
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| seller_id   | int     |
+| product_id  | int     |
+| buyer_id    | int     |
+| sale_date   | date    |
+| quantity    | int     |
+| price       | int     |
++-------------+---------+
+This table can have duplicate rows.
+product_id is a foreign key (reference column) to the Product table.
+Each row of this table contains some information about one sale.
+ 
+
+Write a solution to report the products that were only sold in the first quarter of 2019. That is, between 2019-01-01 and 2019-03-31 inclusive.
+
+Return the result table in any order.
+
+The result format is in the following example.
+'''
+  
+SELECT DISTINCT p.product_id, p.product_name
+FROM product p
+JOIN sales s ON p.product_id = s.product_id
+WHERE s.sale_date BETWEEN '2019-01-01' AND '2019-03-31'
+AND s.product_id NOT IN (
+    SELECT product_id
+    FROM sales
+    WHERE sale_date NOT BETWEEN '2019-01-01' AND '2019-03-31'
+);
