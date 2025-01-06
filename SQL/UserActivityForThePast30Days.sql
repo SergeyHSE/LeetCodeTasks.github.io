414790f0c9bf71f188d52dd952feceb01db902ed
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2025-01-06 18:09:16 +0300

    Update UserActivityForThePast30Days.sql

--- SQL/UserActivityForThePast30Days.sql
+++ SQL/UserActivityForThePast30Days.sql
@@ -15,8 +15,10 @@ The table shows the user activities for a social media website.
 Note that each session belongs to exactly one user.
  
 
-Write a solution to find the daily active user count for a period of 30 days ending 2019-07-27 inclusively. A user was active on someday if they made at least one activity on that day.
-
+Write a solution to find the daily active user count for a period of 30 days ending 2019-07-27 inclusively.
+A user was active on someday if they made at least one activity on that day.
+'''
+ 
 # Write your MySQL query statement below
 select activity_date as day, count(distinct user_id) as active_users
 from activity
