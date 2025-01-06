6e0692ebe0d1cef5ec4d005bb916bf67e1b877a2
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2025-01-06 18:15:37 +0300

    Update and rename ArticleViews.sql to ArticleViews1.sql

--- SQL/ArticleViews1.sql
+++ SQL/ArticleViews1.sql
@@ -19,3 +19,9 @@ Write a solution to find all the authors that viewed at least one of their own a
 Return the result table sorted by id in ascending order.
 '''
 
+# Write your MySQL query statement below
+select author_id as id
+from views
+where author_id = viewer_id
+group by author_id
+order by author_id;
