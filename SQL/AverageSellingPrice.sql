ccf8574ec7e26324f82369279c1d6e7016c8b090
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2025-01-07 18:21:41 +0300

    Update AverageSellingPrice.sql

--- SQL/AverageSellingPrice.sql
+++ SQL/AverageSellingPrice.sql
@@ -30,3 +30,15 @@ Each row of this table indicates the date, units, and product_id of each product
 Write a solution to find the average selling price for each product. average_price should be rounded to 2 decimal places. If a product does not have any sold units, its average selling price is assumed to be 0.
 '''
 
+# Write your MySQL query statement below
+SELECT 
+    p.product_id,
+    ROUND(SUM(u.units * p.price) * 1.0 / NULLIF(SUM(u.units), 0), 2) AS average_price
+FROM 
+    Prices p
+LEFT JOIN 
+    UnitsSold u
+ON 
+    p.product_id = u.product_id
+    AND u.purchase_date BETWEEN p.start_date AND p.end_date
+GROUP BY p.product_id;
