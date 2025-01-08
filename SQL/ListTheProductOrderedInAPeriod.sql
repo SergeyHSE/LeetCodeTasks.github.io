a5804526558ac0534491d50091de5edeeadca018
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2025-01-08 17:23:36 +0300

    Update ListTheProductOrderedInAPeriod.sql

--- SQL/ListTheProductOrderedInAPeriod.sql
+++ SQL/ListTheProductOrderedInAPeriod.sql
@@ -29,3 +29,10 @@ unit is the number of products ordered in order_date.
 Write a solution to get the names of products that have at least 100 units ordered in February 2020 and their amount.
 '''
 
+# Write your MySQL query statement below
+select p.product_name, sum(o.unit) unit
+from products p
+join orders o on p.product_id = o.product_id
+where o.order_date >= '2020-02-01' and o.order_date <= '2020-02-29'
+group by p.product_name
+having sum(o.unit) >= 100;
