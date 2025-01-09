f79e889578c3a84b0f26ecc0cef07562f5694488
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2025-01-09 14:13:01 +0300

    Update CustomerWhoVisitedButDidNotMakeAnyTransaction.sql

--- SQL/CustomerWhoVisitedButDidNotMakeAnyTransaction.sql
+++ SQL/CustomerWhoVisitedButDidNotMakeAnyTransaction.sql
@@ -27,3 +27,9 @@ This table contains information about the transactions made during the visit_id.
 Write a solution to find the IDs of the users who visited without making any transactions and the number of times they made these types of visits.
 '''
 
+# Write your MySQL query statement below
+select v.customer_id, sum(ifnull(t.transaction_id, 1)) count_no_trans
+from visits v
+left join transactions t on v.visit_id = t.visit_id
+where t.transaction_id is null
+group by customer_id;
