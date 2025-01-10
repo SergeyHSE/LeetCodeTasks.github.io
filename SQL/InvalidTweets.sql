470635bf166e90112b29e686a760b27f723ab71d
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2025-01-10 12:10:21 +0300

    Update InvalidTweets.sql

--- SQL/InvalidTweets.sql
+++ SQL/InvalidTweets.sql
@@ -16,3 +16,7 @@ Write a solution to find the IDs of the invalid tweets.
 The tweet is invalid if the number of characters used in the content of the tweet is strictly greater than 15.
 '''
 
+# Write your MySQL query statement below
+select tweet_id
+from tweets
+where length(content) > 15;
