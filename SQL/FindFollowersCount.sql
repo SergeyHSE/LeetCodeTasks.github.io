997e3a887f7e504a3aeacaafa3fbbc481e604332
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2025-01-10 13:22:27 +0300

    Update FindFollowersCount.sql

--- SQL/FindFollowersCount.sql
+++ SQL/FindFollowersCount.sql
@@ -16,3 +16,8 @@ Write a solution that will, for each user, return the number of followers.
 Return the result table ordered by user_id in ascending order.
 '''
 
+# Write your MySQL query statement below
+select user_id, count(follower_id) followers_count
+from followers
+group by user_id
+order by user_id;
