2f6340c7fd36ab521c1a1bc94d27a8f8aacd7793
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2025-01-11 15:40:13 +0300

    Update RearrangeProductTable.sql

--- SQL/RearrangeProductTable.sql
+++ SQL/RearrangeProductTable.sql
@@ -16,3 +16,17 @@ If the product is not available in a store, the price will be null in that store
 Write a solution to rearrange the Products table so that each row has (product_id, store, price).
 If a product is not available in a store, do not include a row with that product_id and store combination in the result table.
 '''
+
+# Write your MySQL query statement below
+select product_id, 'store1' as store, store1 as price
+from products
+where store1 is not null
+union
+select product_id, 'store2' as store, store2 as price
+from products
+where store2 is not null
+union
+select product_id, 'store3' as store, store3 as price
+from products
+where store3 is not null;
+
