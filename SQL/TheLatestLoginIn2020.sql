5829993d149673971d452bdf170f7475f567dc0e
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2025-01-12 14:23:43 +0300

    Update TheLatestLoginIn2020.sql

--- SQL/TheLatestLoginIn2020.sql
+++ SQL/TheLatestLoginIn2020.sql
@@ -13,3 +13,12 @@ Each row contains information about the login time for the user with ID user_id.
 Write a solution to report the latest login for all users in the year 2020. Do not include the users who did not login in 2020.
 '''
 
+/* Write your T-SQL query statement below */
+with logins_cte as (
+    select user_id, time_stamp
+    from logins
+    where year(time_stamp) = 2020
+)
+select user_id, max(time_stamp) as last_stamp
+from logins_cte
+group by user_id;
