d760f0e054df4ac6e4bbca10a5cb02e875c17ae3
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2025-01-13 13:46:11 +0300

    Update FirstUniqueCharacterInAString.py

--- Easy/FirstUniqueCharacterInAString.py
+++ Easy/FirstUniqueCharacterInAString.py
@@ -1,27 +1,33 @@
 '''
 Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
 
- 
-
 Example 1:
-
 Input: s = "leetcode"
-
 Output: 0
-
 Explanation:
-
 The character 'l' at index 0 is the first character that does not occur at any other index.
 
 Example 2:
-
 Input: s = "loveleetcode"
-
 Output: 2
 
 Example 3:
-
 Input: s = "aabb"
-
 Output: -1
 '''
+
+class Solution(object):
+    def firstUniqChar(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+    dict_characters = {}
+    for i in s:
+        dict_characters[i] = dict_characters.get(i, 0) + 1
+
+    for j in range(len(s)):
+        if dict_characters[s[i]] == 1:
+            return i
+
+    return -1 
