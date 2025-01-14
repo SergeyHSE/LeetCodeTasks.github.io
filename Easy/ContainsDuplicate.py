9c9e6a1dcc812455a35eafd22c0e7fe27404506c
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2025-01-14 21:00:35 +0300

    Update ContainsDuplicate.py

--- Easy/ContainsDuplicate.py
+++ Easy/ContainsDuplicate.py
@@ -14,3 +14,19 @@ class Solution(object):
                 return True
             set_nums.add(num)
         return False
+
+# Another
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] == nums[j]:
+                    return True
+
+        return False
+
