0a7c81657c0e0ac4831093c3301bf1af68944830
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2025-01-16 15:20:12 +0300

    Update LongestCommonPrefix.py

--- Easy/LongestCommonPrefix.py
+++ Easy/LongestCommonPrefix.py
@@ -20,3 +20,19 @@ class Solution(object):
                     return f_s[:i]
 
         return f_s 
+
+# Anpther solution
+
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ""
+        prefix = strs[0]
+        for i in range(1, len(strs)):
+            while strs[i].find(prefix) != 0:
+                prefix = prefix[:-1]
+        return prefix
