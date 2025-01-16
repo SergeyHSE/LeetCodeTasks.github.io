5514b3f88e9a1ada6ae789e2755869d4e3b16c26
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2025-01-16 14:54:20 +0300

    Update RomanToInteger.py

--- Easy/RomanToInteger.py
+++ Easy/RomanToInteger.py
@@ -26,3 +26,32 @@ class Solution(object):
                 r += v
             v_p = v
         return r
+
+# Another solution
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        r_d = {
+            'I' : 1,
+            'V' : 5,
+            'X' : 10,
+            'L' : 50,
+            'C' : 100,
+            'D' : 500,
+            'M' : 1000}
+
+        s = list(s)
+
+        c = r_d[s[0]]
+        r = r_d[s[0]]
+
+        for i in s[1:]:
+            if r_d[i] > r:
+                c += r_d[i] - 2 * r
+            else:
+                c += r_d[i]
+            r = r_d[i]
+        return c
