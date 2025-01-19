8da2d58470f16b240c9780387405cb7253742d2e
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2025-01-19 18:36:51 +0300

    Update ThirdMaximumNumber.py

--- Easy/ThirdMaximumNumber.py
+++ Easy/ThirdMaximumNumber.py
@@ -3,3 +3,16 @@ Given an integer array nums, return the third distinct maximum number in this ar
 If the third maximum does not exist, return the maximum number.
 '''
 
+class Solution(object):
+    def thirdMax(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums = list(set(nums))
+        nums.sort(reverse = True)
+        if len(nums) < 3:
+            return nums[0]
+        else:
+            return nums[2]
+          
