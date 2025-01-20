0cf65479157b1f2f95edf3068f7b7537e8fc7315
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2025-01-20 11:02:18 +0300

    Update FizzBuzz.py

--- Easy/FizzBuzz.py
+++ Easy/FizzBuzz.py
@@ -6,3 +6,22 @@ answer[i] == "Fizz" if i is divisible by 3.
 answer[i] == "Buzz" if i is divisible by 5.
 answer[i] == i (as a string) if none of the above conditions are true.
 '''
+
+class Solution(object):
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        result = []
+        list_number = list(range(1, n+1))
+        for i in list_number:
+            if i % 3 == 0 and i % 5 == 0:
+                result.append("FizzBuzz")
+            elif i % 3 == 0:
+                result.append("Fizz")
+            elif i % 5 == 0:
+                result.append("Buzz")
+            else:
+                result.append(str(i))
+        return result
