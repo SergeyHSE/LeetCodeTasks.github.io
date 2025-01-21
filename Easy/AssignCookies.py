35f1567705b918138a4eba61240b7caf49aa8ef2
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2025-01-21 10:42:17 +0300

    Update AssignCookies.py

--- Easy/AssignCookies.py
+++ Easy/AssignCookies.py
@@ -4,3 +4,22 @@ Each child i has a greed factor g[i], which is the minimum size of a cookie that
 If s[j] >= g[i], we can assign the cookie j to the child i, and the child i will be content.
 Your goal is to maximize the number of your content children and output the maximum number.
 '''
+
+class Solution(object):
+    def findContentChildren(self, g, s):
+        """
+        :type g: List[int]
+        :type s: List[int]
+        :rtype: int
+        """
+        g.sort()
+        s.sort()
+        child_idx = 0
+        cookie_idx = 0
+        while child_idx < len(g) and cookie_idx < len(s):
+            if s[cookie_idx] >= g[child_idx]:
+                child_idx += 1
+                cookie_idx += 1
+            else:
+                cookie_idx += 1
+        return child_idx
