3eceeb1021225ae2a1180822d47bda0913c34089
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2025-01-27 15:02:49 +0300

    Update NthHighestSalary.sql

--- SQL/NthHighestSalary.sql
+++ SQL/NthHighestSalary.sql
@@ -3,6 +3,7 @@ Write a solution to find the nth highest salary from the Employee table. If ther
 The result format is in the following example.
 '''
 
+# first solution
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
     set N = N - 1;
@@ -12,5 +13,20 @@ BEGIN
         order by salary desc
         limit 1 offset N
 
+    );
+END
+
+# second solution
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+    RETURN (
+        select salary
+        from (select salary,
+            dense_rank() over(
+                order by salary desc) as n_th
+        from employee) as salary_number
+
+        where n_th = n
+        limit 1
     );
 END
