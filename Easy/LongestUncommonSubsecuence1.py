9ec20b618f6e74ccbce2c108c69a7816fccafb3c
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2025-01-27 10:44:43 +0300

    Create LongestUncommonSubsecuence1.py

---
+++ Easy/LongestUncommonSubsecuence1.py
@@ -0,0 +1,5 @@
+'''
+Given two strings a and b, return the length of the longest uncommon subsequence between a and b. If no such uncommon subsequence exists, return -1.
+An uncommon subsequence between two strings is a string that is a subsequence of exactly one of them.
+'''
+
