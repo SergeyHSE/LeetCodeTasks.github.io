4347dbf14fc87b533a70b40666efe5ccf7ca1ff3
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2025-01-27 11:10:13 +0300

    Update LongestUncommonSubsecuence1.py

--- Easy/LongestUncommonSubsecuence1.py
+++ Easy/LongestUncommonSubsecuence1.py
@@ -3,3 +3,19 @@ Given two strings a and b, return the length of the longest uncommon subsequence
 An uncommon subsequence between two strings is a string that is a subsequence of exactly one of them.
 '''
 
+class Solution(object):
+    def findLUSlength(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: int
+        """
+        if len(a) != len(b):
+            return max(len(a), len(b))
+
+        for i in range(max(len(a), len(b))):
+
+            if a[i] != b[i]: 
+                return max(len(a), len(b))
+        
+        return -1 
