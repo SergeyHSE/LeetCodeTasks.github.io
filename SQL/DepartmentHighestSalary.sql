4f73fca5c0b71c7cff70e355bfcd237cb02b3214
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2025-01-28 09:51:33 +0300

    Create DepartmentHighestSalary.sql

---
+++ SQL/DepartmentHighestSalary.sql
@@ -0,0 +1,41 @@
+'''
+Table: Employee
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| id           | int     |
+| name         | varchar |
+| salary       | int     |
+| departmentId | int     |
++--------------+---------+
+id is the primary key (column with unique values) for this table.
+departmentId is a foreign key (reference columns) of the ID from the Department table.
+Each row of this table indicates the ID, name, and salary of an employee. It also contains the ID of their department.
+ 
+
+Table: Department
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
++-------------+---------+
+id is the primary key (column with unique values) for this table. It is guaranteed that department name is not NULL.
+Each row of this table indicates the ID of a department and its name.
+ 
+
+Write a solution to find employees who have the highest salary in each of the departments.
+
+Return the result table in any order.
+
+The result format is in the following example.
+'''
+
+select d.name as department, e.name as employee, e.salary as salary
+from employee e
+join department d on e.departmentid = d.id
+where e.salary = (select max(e1.salary)
+                from employee e1
+                where e1.departmentid = e.departmentid);
