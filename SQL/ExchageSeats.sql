5b3b990f2162fef9a605c23659062bc6653ddcba
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2025-02-06 11:10:38 +0300

    Update ExchageSeats.sql

--- SQL/ExchageSeats.sql
+++ SQL/ExchageSeats.sql
@@ -16,3 +16,13 @@ Write a solution to swap the seat id of every two consecutive students. If the n
 
 Return the result table ordered by id in ascending order.
 '''
+
+# Write your MySQL query statement below
+with cte_seat as (select *, case
+    when id % 2 != 0 then id+1
+    else id-1
+end as new_id
+from seat
+order by new_id)
+select rank() over (order by new_id) as id, student
+from cte_seat;
