68046b1889f8c3636083e5ac3fd8275952e33d05
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2025-02-12 16:10:26 +0300

    Update MonthlyTransactions1.sql

--- SQL/MonthlyTransactions1.sql
+++ SQL/MonthlyTransactions1.sql
@@ -19,12 +19,17 @@ Write an SQL query to find for each month and country, the number of transaction
 '''
 
 # Write your MySQL query statement below
-with cte_transactions as (select id, country, state, amount, date_format(trans_date, '%Y-%m') as month
-from transactions) 
-select month, country, count(id) as trans_count, sum(case 
+with cte_transactions as (
+    select id, country, state, amount, date_format(trans_date, '%Y-%m') as month
+    from transactions
+)
+ 
+select month, country, count(id) as trans_count, sum(
+    case 
         when state = 'approved' then 1
         else 0
-    end) as approved_count, sum(amount) as trans_total_amount, sum(case 
+    end) as approved_count, sum(amount) as trans_total_amount, sum(
+    case 
         when state = 'approved' then amount
         else 0
     end) as approved_total_amount
