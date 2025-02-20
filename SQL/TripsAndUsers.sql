02e1085c99b2c2ccf82c58622377da63c8eb2ab7
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2025-02-20 12:33:43 +0300

    Update TripsAndUsers.sql

--- SQL/TripsAndUsers.sql
+++ SQL/TripsAndUsers.sql
@@ -27,3 +27,16 @@ banned is an ENUM (category) type of ('Yes', 'No').
 The cancellation rate is computed by dividing the number of canceled (by client or driver) requests with unbanned users by the total number of requests with unbanned users on that day.
 Write a solution to find the cancellation rate of requests with unbanned users (both client and driver must not be banned) each day between "2013-10-01" and "2013-10-03". Round Cancellation Rate to two decimal points.
 '''
+
+# Write your MySQL query statement below
+with cte_trips as(select t.id, t.client_id, t.status, t.request_at,
+    u.banned
+    from trips t
+    left join users u on t.client_id = u.users_id
+    left join users u2 on t.driver_id = u2.users_id
+    where u.banned != 'Yes'
+    and u2.banned != 'Yes'
+    and request_at between '2013-10-01' and '2013-10-03')
+select request_at as day, round(sum(case when status like 'cancelled_by_%' then 1 else 0 end) / count(request_at), 2) as "cancellation rate"
+from cte_trips
+group by day;
